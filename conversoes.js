1411c58ae82c940420ee496ebd14346fe4506836
ceciliakovalek-maker <cecilia.kovalek@escola.pr.gov.br>
2026-04-09 14:53:02 +0000

    .

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+//tipo de dados
+//boleanos 
+
+//conversão implícita
+const numero = 456;
+const numeroString = Number("456");
+
+//nuber()
+//string()
+console.log(numero + numeroString);
+
+//conversão explícita
