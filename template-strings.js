529d5eb068a2b15c0b37b35244249739d1775ebd
ceciliakovalek-maker <cecilia.kovalek@escola.pr.gov.br>
2026-05-14 15:20:27 +0000

    ~

--- template-strings.js
+++ template-strings.js
@@ -1,8 +1,8 @@
 const nome = "Cecilia ";
 const idade = 2026-2009;
 const cidadeDeNacimento = "Roncador";
-//const apresentacao = "meu nome é " + nome + "minha idade é " + idade + " e nasci na cidade " + cidadeDeNacimento;
+//const apresacao = `meu nome é ${nome}, minha idade é ${idade} e na cidade de ${cidadeDeNacimento}`;
+entacao = "meu nome é " + nome + "minha idade é " + idade + " e nasci na cidade " + cidadeDeNacimento;
 
-const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e na cidade de ${cidadeDeNacimento}`;
-
+const apresent
 console.log(apresentacao);
